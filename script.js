6e81083f1fa84078782f351b127db35ac490c531
weekiatlim022 <weekiat0539@gmail.com>
2023-02-18 13:48:51 +0800

    add portfolio section

--- script.js
+++ script.js
@@ -62,4 +62,18 @@ tabs.forEach(tab =>{
         })
         tab.classList.add('qualification__active');
     })
-})
+});
+
+//portfolio swiper
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
